c4fa74f60f84b3eb6ba9a1f79b29b665ac69465c
Alex Macniven <macniven.ap@gmail.com>
2022-03-05 13:14:04 +0000

    Add Error When Name Is Empty

--- greetings.go
+++ greetings.go
@@ -1,13 +1,21 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Function has a parameter called name of type string
-// And a return type of string
-func Hello(name string) string {
+// And a return type of string and error
+func Hello(name string) (string, error) {
+	// When an empty value is supplied as name
+	// Return an empty string and a new error
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	// The := operator initiates and assigns a value to a variable
 	// Sprintf returns a string with formatting applied
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	// Return the message, which is the same type as the return signature
-	return message
+	// Return the message and an empty error
+	return message, nil
 }
