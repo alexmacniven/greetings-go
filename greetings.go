8f058e64a5a27f7756ab022ff72259879f00ec90
Alex Macniven <macniven.ap@gmail.com>
2022-03-05 14:03:38 +0000

    Add Random Greetings

--- greetings.go
+++ greetings.go
@@ -3,7 +3,23 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+// init function called when a package is imported.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v. Well met!",
+	}
+	return formats[rand.Intn(len(formats))]
+}
 
 // Function has a parameter called name of type string
 // And a return type of string and error
@@ -15,7 +31,7 @@ func Hello(name string) (string, error) {
 	}
 	// The := operator initiates and assigns a value to a variable
 	// Sprintf returns a string with formatting applied
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	// Return the message and an empty error
 	return message, nil
 }
